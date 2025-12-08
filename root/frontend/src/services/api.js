99e6a9547ae06694dfe71e9adc417ff42c58a0e6
damaleparas <damaleparas@gmail.com>
2025-12-08 12:26:08 +0530

    added the backend url

--- root/frontend/src/services/api.js
+++ root/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = ' https://truestate-backend-qhfb.onrender.com/api';
 
 // 1. Fetch Sales (Used for the Table)
 export const fetchSales = async (params) => {
